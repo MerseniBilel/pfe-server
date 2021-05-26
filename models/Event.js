2b4bb691bf67a6e82b0f7ba899fbf119f2229747
MerseniBilel <bilelmerseni7016@gmail.com>
2021-05-26 16:33:16 +0100

    event model start

---
+++ models/Event.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+//create evnet model
+
+const eventSchema = mongoose.Schema({
+  
+});
+
+
+module.exports = Events = mongoose.model('event',eventSchema);
